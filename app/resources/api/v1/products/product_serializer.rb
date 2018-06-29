4f7816b531ca3df56649057d9e29e7eddd7c6ee7
Kelvin <kelvin932810@gmail.com>
2018-06-29 16:54:14 -0600

    :D

--- app/resources/api/v1/products/product_serializer.rb
+++ app/resources/api/v1/products/product_serializer.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     module Products
       class ProductSerializer < ActiveModel::Serializer
-        attributes :id, :code, :name, :sale_price, :stock_quantity
+        attributes :id, :code, :name, :sale_price, :stock_quantity, :likes_count
       end
     end
   end
